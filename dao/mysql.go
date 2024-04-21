50ce5d358ddcd02d15ef7c738e1820ee0bcaeb8d
pony <pony@yishikj.cn>
2024-04-21 10:45:55 +0800

    go博客

--- dao/mysql.go
+++ dao/mysql.go
@@ -13,7 +13,7 @@ var DB *sql.DB
 
 func init() {
 	//执行main之前 先执行init方法
-	dataSourceName := fmt.Sprintf("root:pony@2024@tcp(47.123.5.226:3306)/goblog?charset=utf8&loc=%s&parseTime=true", url.QueryEscape("Asia/Shanghai"))
+	dataSourceName := fmt.Sprintf("root:root@tcp(localhost:3306)/goblog?charset=utf8&loc=%s&parseTime=true", url.QueryEscape("Asia/Shanghai"))
 	db, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
 		log.Println("连接数据库异常")
